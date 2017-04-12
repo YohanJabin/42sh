410482bf3c9c309953e2a3918b93b6d05ceaaebb
Hugo <hugo.martin@epitech.eu>
2017-04-12 23:09:12 +0200

    last

--- src/start.c
+++ src/start.c
@@ -5,17 +5,17 @@
 ** Login	hugo.martin@epitech.eu
 **
 ** Started on	Wed Apr 12 17:51:07 2017 Hugo MARTIN
-** Last update	Wed Apr 12 20:10:10 2017 Hugo MARTIN
+** Last update	Wed Apr 12 22:29:48 2017 Hugo MARTIN
 */
 
 #include "my.h"
 
 int	my_start(t_my_var *p)
 {
-  //exec de .42shrc ?
+  //exec de .42shrc?
   while (42)
   {
-    //prompt
+    //prompt perso
     //command/list:
     my_command(p);
     /*FIXME*/aff_list(p->list_command);
