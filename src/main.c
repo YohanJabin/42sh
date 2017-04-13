1d62f3ecb40da59ac67b738622d836fe6c2d176a
adrien <adrien.zemma@epitech.eu>
2017-04-13 20:47:25 +0200

    lecture entre 0

--- src/main.c
+++ src/main.c
@@ -5,16 +5,21 @@
 ** Login	adrien.zemma@epitech.eu
 **
 ** Started on	Wed Apr 12 12:54:03 2017 Adrien ZEMMA
-** Last update	Thu Apr 13 11:09:58 2017 Hugo MARTIN
+** Last update	Thu Apr 13 20:46:52 2017 Adrien ZEMMA
 */
 
 #include "my.h"
 
 int	main(int ac, char **av, char **env)
 {
+  int nb;
+  char buff[4096];
   t_my_var	*p;
 
   p = malloc(sizeof(*p));
+  nb = read(0, buff, 4095);
+  buff[nb] = '\0';
+  my_printf("%s", buff);
   ac = ac;
   av = av;
   p->return_value = 0;
