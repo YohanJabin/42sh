31eaa848a207c23b19d1316a66125f24abfd16cb
Hugo <hugo.martin@epitech.eu>
2017-04-13 11:09:15 +0200

    no error of compil and you love my english isn't it ?

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login	adrien.zemma@epitech.eu
 **
 ** Started on	Wed Apr 12 12:54:03 2017 Adrien ZEMMA
-** Last update	Wed Apr 12 20:05:19 2017 Hugo MARTIN
+** Last update	Thu Apr 13 11:09:10 2017 Hugo MARTIN
 */
 
 #include "my.h"
@@ -13,12 +13,8 @@
 int	main(int ac, char **av, char **env)
 {
   int nb;
-  char buff[8046];
   t_my_var	*p;
 
-  nb = read(0, buff, 8046);
-  buff[nb] = '\0';
-  my_printf("%s\n", buff);
   p = malloc(sizeof(*p));
   ac = ac;
   av = av;
