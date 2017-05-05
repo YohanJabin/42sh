f3f9907d7cc82b8675dbe65fc2a88846b3a1b27c
Yohan.Jabin <yohan.jabin@epitech.eu>
2017-05-05 13:05:14 +0200

    where

--- src/builtins/which.c
+++ src/builtins/which.c
@@ -5,12 +5,12 @@
 ** Login   <yohan.jabin@epitech.eu>
 ** 
 ** Started on  Thu May  4 21:25:49 2017 Yohan.Jabin
-** Last update Thu May  4 21:50:18 2017 Yohan.Jabin
+** Last update Fri May  5 13:04:09 2017 Yohan.Jabin
 */
 
 #include "my.h"
 
-void	search_cmd_path(t_my_var *data, char *cmd)
+void	search_cmd_path(t_my_var *data, char *cmd, int flag)
 {
   char	**arr_path;
   char	*path;
@@ -24,14 +24,18 @@ void	search_cmd_path(t_my_var *data, char *cmd)
       if (access(path, F_OK) == 0)
 	{
 	  my_printf("%s\n", path);
-	  return ;
+	  if (flag == 1)
+	    return ;
 	}
     }
-  my_fprintf(2, "%s: Command not found.\n", cmd);
-  data->return_value = 1;
+  if (flag == 1)
+    {
+      my_fprintf(2, "%s: Command not found.\n", cmd);
+      data->return_value = 1;
+    }
 }
 
-void	search_cmd(t_my_var *data, char *cmd)
+void	search_cmd(t_my_var *data, char *cmd, int flag)
 {
   if (my_strncmp(cmd, "set", my_strlen(cmd)) == 1
       || my_strncmp(cmd, "unset", my_strlen(cmd)) == 1
@@ -41,21 +45,26 @@ void	search_cmd(t_my_var *data, char *cmd)
       || my_strncmp(cmd, "setenv", my_strlen(cmd)) == 1
       || my_strncmp(cmd, "unsetenv", my_strlen(cmd)) == 1
       || my_strncmp(cmd, "which", my_strlen(cmd)) == 1
+      || my_strncmp(cmd, "where", my_strlen(cmd)) == 1
       || my_strncmp(cmd, "echo", my_strlen(cmd)) == 1)
     {
-      my_printf("%s: shell built-in command.\n");
-      return ;
+      if (flag == 1)
+	{
+	  my_printf("%s: shell built-in command.\n", cmd);
+	  return ;
+	}
+      my_printf("%s is a shell built-in\n", cmd);
     }
-  search_cmd_path(data, cmd);
+  search_cmd_path(data, cmd, flag);
 }
 
-void	loop_which(t_my_var *data, char **imp)
+void	loop_which(t_my_var *data, char **imp, int flag)
 {
   int	i;
 
   i = 0;
   while (imp[++i] != NULL)
-    search_cmd(data, imp[i]);
+    search_cmd(data, imp[i], flag);
 }
 
 int	test_cmd_which(t_my_var *data, char **imp)
@@ -68,7 +77,12 @@ int	test_cmd_which(t_my_var *data, char **imp)
 	  data->return_value = 1;
 	}
       else
-	loop_which(data, imp);
+	loop_which(data, imp, 1);
+      return (0);
+    }
+  else if (my_strncmp(imp[0], "where", 6) == 1)
+    {
+      loop_which(data, imp, 2);
       return (0);
     }
   return (1);
