f86ee4948a4b1f0938bf7502f8e7da044cd4465d
Hugo <hugo.martin@epitech.eu>
2017-05-19 17:05:16 +0200

    all

--- src/setup/start.c
+++ src/setup/start.c
@@ -5,21 +5,35 @@
 ** Login   <hugo.martin@epitech.eu>
 **
 ** Started on  Fri Apr 28 16:11:10 2017 Hugo
-** Last update	Fri May 19 13:21:41 2017 Hugo MARTIN
+** Last update	Fri May 19 17:05:09 2017 Hugo MARTIN
 */
 
 #include "my.h"
-#include "my_prompt.h"
+
 //TODO
-int	my_control(t_my_var *p, t_my_separator *s)
+int	my_control(t_my_var *p, t_my_separator *s, t_my_list_data	*separator)
 {
   //my_printf("Command: %s\nFlag: %d\nReturn Value: %d\n\n", s->command, s->flag, p->return_value);
-  if (p->return_value != 0 && s->flag == 1)
-    return (-1);
-  else if (p->return_value != 0 && s->flag == 2)
-    return (0);
-  else if (p->return_value == 0 && s->flag == 2)
-    return (-1);
+  if (s->flag == 1)
+  {
+    if (p->return_value != 0)
+    {
+      p->return_value = 1;
+      if (separator->next == NULL)
+        p->return_value = 0;
+      return (-1);
+    }
+  }
+  else if (s->flag == 2)
+  {
+    if (p->return_value == 0)
+    {
+      p->return_value = 1;
+      if (separator->next == NULL)
+        p->return_value = 0;
+      return (-1);
+    }
+  }
   return (0);
 }
 
@@ -41,14 +55,17 @@ int	my_start(t_my_var *p, t_my_prompt *prompt)
        if (p->separator != NULL)
        {
         separator = p->separator->begin;
-        while (separator && (tmp = (t_my_separator *) separator->data) &&
-        my_control(p, tmp) == 0 && (p->return_value = 0) == 0)
+        while (separator && (tmp = (t_my_separator *) separator->data))
         {
-          update_env(p);
-          my_histori(tmp->command, prompt);
-          if (parse_pipe(p, tmp->command) == 84)
+          if (my_control(p, tmp, separator) == 0)
+          {
+            p->return_value = 0;
+            update_env(p);
+            my_histori(tmp->command, prompt);
+            if (parse_pipe(p, tmp->command) == 84)
             return (84);
-          separator = separator->next;
+          }
+        separator = separator->next;
         }
         if (p->exit != -1)
         	exit(p->return_value);
